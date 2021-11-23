4e09714c3f0c85cfc1796a8b5dc3129ba04e4e4e
cihan sungur <sungur88@windowslive.com>
2021-11-23 22:36:07 +0300

    Ders

--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -1,6 +1,10 @@
+using Identity.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +16,25 @@ namespace Identity
 {
     public class Startup
     {
+
+        public IConfiguration configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration; 
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString"));
+            });
+
+            services.AddIdentity<AppUser,IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();  
+
+
             services.AddMvc(option=>
             option.EnableEndpointRouting=false);
         }
@@ -27,6 +46,7 @@ namespace Identity
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+            app.UseAuthentication();
         }
     }
 }
