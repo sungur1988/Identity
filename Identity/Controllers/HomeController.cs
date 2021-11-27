c52f79582078d743dce4530a9c2c859713baa864
cihan sungur <sungur88@windowslive.com>
2021-11-27 23:47:19 +0300

    Ders 10

--- Identity/Controllers/HomeController.cs
+++ Identity/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Identity.Models;
+using Identity.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,52 @@ namespace Identity.Controllers
 {
     public class HomeController : Controller
     {
+        private UserManager<AppUser> _userManager { get; }
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser();
+                appUser.UserName = userViewModel.UserName;
+                appUser.Email = userViewModel.Email;
+                appUser.PhoneNumber = userViewModel.PhoneNumber;
+                IdentityResult result = await _userManager.CreateAsync(appUser, userViewModel.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+
+            }
+            return View(userViewModel);
         }
     }
 }
---
+++ Identity/ViewModels/UserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage ="Kullanıcı adı boş olamaz.")]
+        [Display(Name = "Kullanıcı Adı: ")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Email adresi boş olamaz.")]
+        [Display(Name ="Email adresi: ")]
+        [EmailAddress(ErrorMessage ="Email adresi doğru formatta olmalıdır.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Şifre alanı boş geçilemez.")]
+        [DataType(DataType.Password)]
+        [Display(Name ="Şifreniz: ")]
+        public string Password { get; set; }
+
+        [Display(Name ="Tel No:")]
+        public string PhoneNumber { get; set; }
+    }
+}
